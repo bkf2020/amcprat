87f55bf55181f8c180d1500b2ed6614b4262f7c5
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-03-10 15:50:23 -0800

    partially implementing answer chekcer

--- checker.js
+++ checker.js
@@ -1,5 +1,50 @@
-function getAnswer() {
-}
-function checkAnswer() {
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+		if(radios[y].checked) return radios[y].value; // return the checked value
 }
 
+function getAnswer( qn ) {
+	btnType = document.getElementById("submitbtn" + qn).name;
+	if(btnType === "amcsb") {
+		return getCheckedValue("answer" + qn);
+	}
+	else { // aimesb
+		return document.getElementById("answer" + qn).value;
+	}
+}
+function checkAnswer( qn ) {
+	var userAnswer = getAnswer( qn );
+	var currentData = problemURLS[qn].split("/");
+	var testType = currentData[0];
+	var answerURL = "";
+	var problemNumber = 0;
+	if(testType === "amc8") {
+		for(var i = 0; i < 2; i++) {
+			answerURL += currentData[i];
+			answerURL += "/";
+		}
+		answerURL += "answers.txt";
+		problemNumber = parseInt(currentData[2].substr(0, 2), 10);
+	}
+	else {
+		for(var i = 0; i < 3; i++) {
+			answerURL += currentData[i];
+			answerURL += "/";
+		}
+		answerURL += "answers.txt";
+		problemNumber = parseInt(currentData[3].substr(0, 2), 10);
+	}
+	
+	var client = new XMLHttpRequest();
+	client.open('GET', answerURL);
+	client.onreadystatechange = function()
+	{
+		if( client.responseText != '' )
+		{
+			var txt = client.responseText.split("\n");
+			console.log(txt[problemNumber - 1]);
+		}
+	}
+	client.send();
+}
